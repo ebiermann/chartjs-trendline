9018e028703e33f710c25e5c0032ec570cf34320
Enrico Biermann <enrico.biermann@akquinet.de>
2025-05-27 06:08:18 +0200

    add chartjs trendline plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {BaseChartDirective} from 'ng2-charts';
+import {Chart, ChartOptions} from 'chart.js';
+import chartTrendline from 'chartjs-plugin-trendline';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BaseChartDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chartjs-trendline';
+
+  protected data = () => {
+    const newVar = {
+      datasets: [{
+        trendlineLinear: {
+          width: 1,
+          lineStyle: 'solid',
+          colorMin: '#555555',
+          colorMax: '#555555',
+          projection: true
+        },
+        backgroundColor: '#a51212',
+        label: 'test',
+        data: [
+          {x: 2, y: 1},
+          {x: 2.5, y: 2},
+          {x: 1.5, y: 3},
+          {x: 2, y: 4}
+        ]
+      }]
+    };
+    console.log(newVar);
+    return newVar;
+  }
+
+  protected config: ChartOptions<'scatter'> = {
+    scales: {
+      x: {
+        type: 'linear',
+        position: 'bottom',
+        min: 0,
+        max: 4,
+        title: {
+          text: 'x',
+        }
+      },
+      y: {
+        type: 'linear',
+        position: 'left',
+        min: 0,
+        title: {
+          text: 'y',
+        }
+      },
+    }
+  };
+
+  constructor() {
+    Chart.register(chartTrendline);
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {NG_CHARTS_CONFIGURATION, withDefaultRegisterables} from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes),
+    {
+      provide: NG_CHARTS_CONFIGURATION,
+      useFactory: () => {
+        return withDefaultRegisterables();
+      }
+    },
+  ]
 };
